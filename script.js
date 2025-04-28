229bc1d28bf3fb770135eb90de1721e2174c4bd1
Choppadandi Sai Krishna Mohan <krishnach2204@gmail.com>
2025-04-28 17:18:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Portfolio Loaded!");
 });
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to my Portfolio!");
+});

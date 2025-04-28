41054b5ebc6ba211c688926ce0d49d9af95538d4
Choppadandi Sai Krishna Mohan <krishnach2204@gmail.com>
2025-04-28 19:10:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Portfolio Loaded!");
-});
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Welcome to my Portfolio!");
-});
